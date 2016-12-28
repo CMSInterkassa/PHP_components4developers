7b32e2aa4a6e0780f73c2494e42003977485a239
CMSInterkassa <cms@interkassa.com>
2016-12-28 14:41:35 +0200

    Chekker+signmaker

---
+++ checker.php
@@ -0,0 +1,39 @@
+<?php
+
+// если в настройках кассы интерфейс в полях URL взаимодействия выставлен POST если же используется GET -
+// тогда в дальше место переменной $_POST используем $_GET
+
+$dataSet = $_POST;
+
+//Для просмотра расширенного ответа
+//$answer = 'Our answer from Interkassa :'.PHP_EOL;
+//foreach ($dataSet as $key => $val) {
+//    $answer .= $key.' => '.$val.PHP_EOL;
+//}
+$needle = $dataSet['ik_sign'];
+
+unset($dataSet['ik_sign']);// удаляем из данных строку подписи
+ksort($dataSet, SORT_STRING); // сортируем по ключам в алфавитном порядке элементы массива
+
+$test_key = 'wVKaWkpgF4AmFuD8'; // В данном случае используется Тестовый ключ так как для проверки мы используем Тестовую платежную систему
+
+array_push($dataSet, $test_key); // добавляем в конец массива ключ
+$signString = implode(':', $dataSet); // конкатенируем значения через символ ":"
+$sign = base64_encode(md5($signString, true)); // берем MD5 хэш в бинарном виде по сформированной строке и кодируем в BASE64
+
+if ($needle == $sign) {
+    $answer .= 'Sign successfully'.PHP_EOL;
+    echo 'OK';
+
+} else {
+
+    $answer .= 'Sign do not match'.PHP_EOL;
+    echo 'Something bad';
+}
+$answer .= 'IK sing :'.$needle.PHP_EOL;
+$answer .= 'locally formed :'.$sign.PHP_EOL;
+
+file_put_contents('answer.txt', $answer);
+
+
+
---
+++ sign.php
@@ -0,0 +1,36 @@
+<?php
+
+$dataSet = array (
+		"ik_co_id" => "5534b1683b1eaf00758b4567",
+		"ik_pm_no" => "47533H-28042015-184310",
+		"ik_pw_via" => "test_interkassa_test_xts",
+		"ik_am" => "8990.00",
+		"ik_cur" => "RUB",
+		"ik_desc" => "Your order No 47533H-28042015",
+		"ik_co_prs_id" => "404025496344",
+		"ik_inv_id" => "36051650",
+		"ik_inv_st" => "success",
+		"ik_inv_crt" => "2015-04-28 18:48:39",
+		"ik_inv_prc" => "2015-04-28 18:48:39",
+		"ik_trn_id" =>"",
+		
+		"ik_co_rfn" => "8810.2000",
+		"ik_ps_price" => "9079.90",
+		
+				
+		);
+$key = "VYZ1aGgkqmzyQshG";
+
+//unset($dataSet['ik_sign']); удаляем из данных строку подписи
+ksort($dataSet, SORT_STRING); // сортируем по ключам в алфавитном порядке элементы массива
+array_push($dataSet, $key); // добавляем в конец массива "секретный ключ"
+$signString = implode(':', $dataSet); // конкатенируем значения через символ ":"
+echo $signString;//вывод строки
+echo "<br>";
+echo "---------- sign ------------";
+echo "<br>";
+$sign = base64_encode(md5($signString, true)); // берем MD5 хэш в бинарном виде по сформированной строке и кодируем в BASE64
+//return $sign; // возвращаем результат
+echo $sign;
+
+?>
